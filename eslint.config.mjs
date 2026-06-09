1f85e6739dc405c30b132d25f38d67b27dfaa9a9
novasugiantara <novasugiantara425@gmail.com>
2026-06-09 10:49:40 +0800

    fix: correct self-closing tags in AppTextInput and TransactionForm components; update ESLint config for HTML self-closing rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,19 @@
 // @ts-check
 import { createConfigForNuxt } from '@nuxt/eslint-config'
 
-export default createConfigForNuxt()
+export default createConfigForNuxt().append({
+    rules: {
+        'vue/html-self-closing': [
+            'warn',
+            {
+                html: {
+                    void: 'never',
+                    normal: 'always',
+                    component: 'always',
+                },
+                svg: 'always',
+                math: 'always',
+            },
+        ],
+    },
+})
